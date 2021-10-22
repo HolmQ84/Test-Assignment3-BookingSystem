aeee58311ac6fcba9d617be14150417c68f34af9
Tureczek <55349594+Tureczek@users.noreply.github.com>
2021-10-22 15:56:50 +0200

    Added a print for employees to the Main class.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
 import datalayer.CustomerStorage.CustomerStorageImpl;
+import datalayer.EmployeeStorage.EmployeeStorageImpl;
 import dto.Customer.Customer;
+import dto.employee.Employee;
 
 import java.sql.SQLException;
 
@@ -13,15 +15,25 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         CustomerStorageImpl storage = new CustomerStorageImpl(conStr, user, pass);
+        EmployeeStorageImpl employeeStorage = new EmployeeStorageImpl(conStr, user, pass);
 
         System.out.println("Got customers: ");
         for(Customer c : storage.getCustomers()) {
             System.out.println(toString(c));
         }
+
+        System.out.println("\nGot Employees: ");
+        for (Employee e : employeeStorage.getEmployees()){
+            System.out.println(toString(e));
+        }
         System.out.println("The end.");
     }
 
+
     public static String toString(Customer c) {
+        return "{" + c.getId() + ", " + c.getFirstname() + ", " + c.getLastname() + "}";
+    }
+    public static String toString(Employee c) {
         return "{" + c.getId() + ", " + c.getFirstname() + ", " + c.getLastname() + "}";
     }
 }
